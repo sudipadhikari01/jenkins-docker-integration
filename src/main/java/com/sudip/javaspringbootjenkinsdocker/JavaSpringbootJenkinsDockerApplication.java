95a4434ecd534a874fada727e297cf9791edcd0a
Sudip Adhikari <31766056+sudipadhikari01@users.noreply.github.com>
2023-01-01 14:41:30 -0600

    first commit project added

---
+++ src/main/java/com/sudip/javaspringbootjenkinsdocker/JavaSpringbootJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.sudip.javaspringbootjenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootJenkinsDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootJenkinsDockerApplication.class, args);
+    }
+
+}
